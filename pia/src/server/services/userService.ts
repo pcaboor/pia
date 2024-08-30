8878a6f9c2104dec716cfe53f623d56c008c36f6
pcaboor <pierre.caboor59@gmail.com>
2024-08-30 12:38:30 +0200

    .env updated

--- pia/src/server/services/userService.ts
+++ pia/src/server/services/userService.ts
@@ -1,6 +1,6 @@
-// src/services/userService.ts
 import bcrypt from 'bcrypt';
 import pool from '../../utils/db'; // Assurez-vous d'avoir une configuration db correcte pour `pool`.
+import { RowDataPacket } from 'mysql2'; // Importation du type RowDataPacket
 
 export const userService = {
   authenticate,
@@ -8,31 +8,39 @@ export const userService = {
 
 async function authenticate(email: string, password: string) {
   const connection = await pool.promise().getConnection();
-
   try {
-    const [rows] = await connection.query(
-      'SELECT * FROM users WHERE email = ?',
-      [email]
-    );
+    // Indiquez explicitement que `rows` est de type `RowDataPacket[]`
+    const [rows] = await connection.query<RowDataPacket[]>('SELECT * FROM users WHERE email = ?', [email]);
 
-    const user = rows[0];
+    if (rows.length > 0) {
+      // Maintenant TypeScript sait que `rows` est un tableau de `RowDataPacket`
+      const userFromDb = rows[0];  
 
-    if (!user) {
-      return null; // Email non trouvé
+      // Transformation de l'utilisateur de la base de données en un format plus utilisable
+      const user = {
+        id: userFromDb.uniqID, // Pas d'erreur car `userFromDb` est correctement typé
+        name: `${userFromDb.firstname} ${userFromDb.lastname}`,
+        email: userFromDb.email,
+        passwordHash: userFromDb.password // On conserve le hash du mot de passe pour la vérification
+      };
+  
+      // Vérification du mot de passe
+      const isValidPassword = await bcrypt.compare(password, user.passwordHash);
+      if (!isValidPassword) {
+        console.log('Mot de passe incorrect.');
+        return null; // Mot de passe incorrect
+      }
+
+      // Si tout va bien, retournez l'utilisateur (ou les données nécessaires seulement)
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      };
+    } else {
+      console.log('Aucun utilisateur trouvé avec cet email.');
+      return null;
     }
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-
-    if (!isValidPassword) {
-      return null; // Mot de passe incorrect
-    }
-
-    // Retourner l'utilisateur avec les informations nécessaires pour la session
-    return {
-      id: user.uniqID,
-      name: `${user.firstName} ${user.lastName}`,
-      email: user.email,
-    };
   } finally {
     connection.release();
   }
